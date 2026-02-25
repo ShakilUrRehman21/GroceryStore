ec59b0d2a7e3c54026b265850a3430bb9eb5964f
SHAKIL UR REHMAN <137157095+ShakilUrRehman21@users.noreply.github.com>
2026-02-25 13:26:59 +0530

    Update settings.py

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tusg+4-qmra*&7ew3go#))ms9!9=x@*mm^z$6figbzmknnvc2*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
